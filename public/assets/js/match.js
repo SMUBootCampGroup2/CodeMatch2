d0918dee097c9fdf831c47177581ff68e38cbad1
peterj330 <peter.a.jarrard@gmail.com>
2018-10-04 20:45:18 -0500

    login functionality, store user info as cookies

--- public/assets/js/match.js
+++ public/assets/js/match.js
@@ -1,20 +1,20 @@
 
 $(document).ready(function() {
 
-    // var loginid = readCookie("userName")
+    var loginid = readCookie("userName")
 
-    // function readCookie(loginid) {
-    //     var nameEQ = loginid + "=";
-    //     var ca = document.cookie.split(";");
-    //     for (var i = 0; i < ca.length; i++) {
-    //       var c = ca[i];
-    //       while (c.charAt(0) === " ") c = c.substring(1, c.length);
-    //       if (c.indexOf(nameEQ) === 0) {
-    //         return c.substring(nameEQ.length, c.length);
-    //       }
-    //     }
-    //     return null;
-    //   }
+    function readCookie(loginid) {
+        var nameEQ = loginid + "=";
+        var ca = document.cookie.split(";");
+        for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) === " ") c = c.substring(1, c.length);
+          if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length, c.length);
+          }
+        }
+        return null;
+      }
 
     // $("#userName").val() = loginid
 
