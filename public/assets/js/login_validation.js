42702f2d299e2a660b7d63ff8a4ddec9f508bc26
peterj330 <peter.a.jarrard@gmail.com>
2018-10-06 09:07:32 -0500

    asdf

--- public/assets/js/login_validation.js
+++ public/assets/js/login_validation.js
@@ -2,19 +2,14 @@ var displayName = "";
 var uid = "";
 var email = "";
 
-
 $(document).ready(function () {
     //	user login event listener
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            //	set user menu data if user is logged in
             displayName = user.displayName;
             uid = user.uid;
             email = user.email;
-            console.log(user.displayName);
-            console.log(user.email);
-            console.log(uid);
-            
+      
             //	usersInfo event listener
             db.ref("users/" + uid + "/userInfo").on("value", function (snapshot) {
                 if (snapshot) {
@@ -29,7 +24,5 @@ $(document).ready(function () {
         } else {
             console.log("else do nothing");
         }
-        // console.log(document.cookie);
-        // console.log(document.cookie[9])
     });
-});		//	End of document.ready()
+});	
